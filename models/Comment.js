5440ec7598f67515303a6cfaf8ec815086609f63
Valeriia <valeriaokh@gmail.com>
2026-04-08 19:46:56 +0200

    refactor comment schema

--- models/Comment.js
+++ models/Comment.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const commentSchema = new mongoose.Schema({
-  post: {
+  postId: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'Post',
     required: [true, "Пост обов'язковий"]
